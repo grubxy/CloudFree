254d9fbf8ad9ce46ad9f3fa38c6b678fc7267b92
叶晓勇 <yexiaoyong@yexiaoyongdeMacBook-Pro.local>
2017-06-30 18:58:24 +0800

    [mod]启动定时任务，修改配置

--- cc/src/main/java/com/xy/Application.java
+++ cc/src/main/java/com/xy/Application.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-//@EnableScheduling
+@EnableScheduling
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
