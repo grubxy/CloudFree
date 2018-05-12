4817e209326811bfdbdbfd72adbc78aed0c6cab1
叶晓勇 <yexiaoyong@yexiaoyongdeMacBook-Pro.local>
2018-05-12 13:07:36 +0800

    update

---
+++ cc/src/main/java/com/xy/domain/SqlInitApplicationRunner.java
@@ -0,0 +1,34 @@
+package com.xy.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SqlInitApplicationRunner implements ApplicationRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    @Override
+    public void run(ApplicationArguments var1) throws Exception {
+        log.info("init db....");
+        Role role = new Role();
+        role.setRid(EnumRole.SYSTEM.getId());
+        role.setRole(EnumRole.SYSTEM.getRole());
+        roleRepository.save(role);
+
+        role = new Role();
+        role.setRid(EnumRole.FLOW.getId());
+        role.setRole(EnumRole.FLOW.getRole());
+        roleRepository.save(role);
+
+        role = new Role();
+        role.setRid(EnumRole.STORE.getId());
+        role.setRole(EnumRole.STORE.getRole());
+        roleRepository.save(role);
+    }
+}
